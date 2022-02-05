5948d764d95aa7339f01419912aaf210e4edfe6a
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-02-05 17:11:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preLoad(){
+
+//useless rn
+
+}
+
+function setup(){
+
+    canvas = createCanvas(700, 600);
+    canvas.position(420, 250);
+
+}
+
+function draw(){
+
+    image(video, 200, 150, 300, 300);
+
+}
